d90ae6c6b4a66e85cde7f3564be5490662ed7e93
Axel Rindle <mail@axelrindle.de>
2023-10-29 11:25:52 +0100

    refactor: remove update check
    I received many comments regarding the Github sign-in
    and decided to remove that feature as VSCode
    automatically updates extensions anyway.
    
    See #4 for details.

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,10 @@
 import { ExtensionContext, Uri, commands } from 'vscode'
 import duplicate from './commands/duplicate'
-import { checkExtensionVersion } from './commands/version-check'
 
 export async function activate(context: ExtensionContext) {
-    await checkExtensionVersion(context)
-
-	context.subscriptions.push(commands.registerCommand(
+    context.subscriptions.push(commands.registerCommand(
         'duplicate-file.execute',
         (uri: Uri) => duplicate(uri))
-    )
-	context.subscriptions.push(commands.registerCommand(
-        'duplicate-file.check-extension-version',
-        () => checkExtensionVersion(context, true))
     )
 }
 
